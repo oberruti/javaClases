5c0fe1ae2572c5f16f8bcfbc943576b1ba9f0c59
Octavio Berruti <octavioberruti@icloud.com>
2022-11-12 12:41:48 -0300

    plantilla added

--- tp/src/main/java/com/javatp/javaTP/database/Plantilla/PlantillaRepository.java
+++ tp/src/main/java/com/javatp/javaTP/database/Plantilla/PlantillaRepository.java
@@ -1,5 +1,6 @@
 package com.javatp.javaTP.database.Plantilla;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,10 +8,15 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import com.javatp.javaTP.database.Jugador.Jugador;
+
 //examples https://javatechonline.com/spring-boot-mongodb-query-examples/
 public interface PlantillaRepository extends MongoRepository<Plantilla, String> {
 
   @Query("{id :?0}")
   Optional<Plantilla> getPlantillaById(String id);
 
+  ArrayList<Plantilla> findByClubID(String ClubID);
+
+  Optional<Plantilla> findByIdAndClubID(String id, String ClubID);
 }
